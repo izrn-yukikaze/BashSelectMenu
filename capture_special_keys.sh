49597281d9451761d212167dca93bb72c67617aa
izurina <nmktkm1112@outlook.jp>
2022-11-14 14:22:16 +0900

    add source code

---
+++ capture_special_keys.sh
@@ -0,0 +1,55 @@
+#!/usr/bin/env bash
+
+capture_keys(){
+  local PUSHED_KEY buff
+  IFS= read -n1 -s PUSHED_KEY
+
+  case "$PUSHED_KEY" in
+    'h')
+      echo "Left"
+      ;;
+    'j')
+      echo "Down"
+      ;;
+    'k')
+      echo "Up"
+      ;;
+    'l')
+      echo "Right"
+      ;;
+
+    $'\x1b')
+      read -r -n2 -s buff
+      PUSHED_KEY+="$buff"
+
+      case "$PUSHED_KEY" in
+	$'\x1b\x5b\x41')
+	  echo "Up"
+	  ;;
+	$'\x1b\x5b\x42')
+	  echo "Down"
+	  ;;
+	$'\x1b\x5b\x43')
+	  echo "Right"
+	  ;;
+	$'\x1b\x5b\x44')
+	  echo "Left"
+	  ;;
+      esac
+      ;;
+
+    "")
+      echo "Enter"
+      ;;
+    $'\x7f')
+      echo "Backspace"
+      ;;
+    $'\x20')
+      echo "Space"
+      ;;
+  esac
+}
+
+key="$(capture_keys)"
+echo $key
+
